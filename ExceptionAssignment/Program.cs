f3f7ee1baed47402d360e7745d4a383bf7eaef9c
JoshOtter <87107050+JoshOtter@users.noreply.github.com>
2021-08-27 09:55:52 -0500

    Created cs files for Exception assignment
    I created a list of integers and had the user input a number that is used in a foreach loop to divide each list element by that number. I put the loop in a try block and made two catch blocks to appropriate error messages when the user inputs 0 or a data type that can't be converted to an integer. I also included a finally block to show the results of the loop and the catch blocks.

---
+++ ExceptionAssignment/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<int> dividends = new List<int>() { 2, 4, 12, 15, 27, 36, 54 };
+        
+        try
+        {
+            Console.WriteLine("Pick a number to divide each number in the dividends list.");
+            int divisor = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("\nHere are the results:");
+            foreach (int dividend in dividends)
+            {
+                Console.WriteLine(dividend / divisor);
+            }
+        }
+        catch (FormatException ex)
+        {
+            Console.WriteLine("Please enter a whole number.");
+        }
+        catch (DivideByZeroException ex)
+        {
+            Console.WriteLine("Please don't divide by zero.");
+        }
+        finally
+        {
+            Console.ReadLine();
+        }
+        
+    }
+}
+
