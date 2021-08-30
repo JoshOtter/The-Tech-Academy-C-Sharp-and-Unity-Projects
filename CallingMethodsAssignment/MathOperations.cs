23e2afafedca5b4eb22331a65f66bf5e3279cc91
JoshOtter <87107050+JoshOtter@users.noreply.github.com>
2021-08-30 10:54:32 -0500

    I created the cs files for the Calling Methods assignment
    I created a class with three methods that took a number input by the user and performed math operations on it. I then called each method twice: once to perform each method's operation on the number, returning all three results, and second to perform all three method's operations on the number in sequence before returning a single result to the user in the console.

---
+++ CallingMethodsAssignment/MathOperations.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethodsAssignment
+{
+    public class MathOperations1
+    {
+        public int argument1 { get; set; }
+        
+        public int Add9()
+        {
+            int result = argument1 + 9;
+            return result;
+        }
+        
+        public int Subtract3()
+        {
+            int result = argument1 - 3;
+            return result;
+        }
+
+        public int Multiply10()
+        {
+            int result = argument1 * 10;
+            return result;
+        }
+    }
+}
---
+++ CallingMethodsAssignment/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethodsAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Based on the instructions, I wasn't sure if I was supposed to give three separate results for each method
+            //or if I was supposed to have it run through each method with only one result, so I did both.
+
+            MathOperations1 userQuery = new MathOperations1();
+
+            //Here is the first option, where each method gives a different result for the same number.
+            Console.WriteLine("Enter an integer.");
+            userQuery.argument1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(userQuery.argument1 + " plus 9 equals: " + userQuery.Add9());
+            Console.WriteLine(userQuery.argument1 + " minus 3 equals: " + userQuery.Subtract3());
+            Console.WriteLine(userQuery.argument1 + " times 10 equals: " + userQuery.Multiply10());
+
+            //Here is the second option, where the number entered moves through each method before being given back to the user.
+            Console.WriteLine("Enter an integer.");
+            int input = Convert.ToInt32(Console.ReadLine());
+            userQuery.argument1 = input;
+
+            userQuery.argument1 = userQuery.Add9();
+            userQuery.argument1 = userQuery.Subtract3();
+            int result = userQuery.Multiply10();
+
+            Console.WriteLine(input + " plus 9, minus 3, multiplied by 10 equals: " + result);
+            Console.ReadLine();
+        }
+    }
+}
