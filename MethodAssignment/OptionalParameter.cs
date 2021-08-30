380315ad34555674a1851f59adc2a9d1c640576e
JoshOtter <87107050+JoshOtter@users.noreply.github.com>
2021-08-30 16:47:59 -0500

    Created the cs files for the Method assignment
    This assignment depended on creating an if statement to see if the optional parameter was entered by the user or if they left it blank.

---
+++ MethodAssignment/OptionalParameter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodAssignment
+{
+    public class OptionalParameter
+    {
+        public int MultiplyBy3(int firstOperand, int secondOperand = 1)
+        {
+            int result = firstOperand * secondOperand * 3;
+            return result;
+        }
+    }
+}
---
+++ MethodAssignment/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            OptionalParameter times3 = new OptionalParameter();
+
+            Console.WriteLine("Enter either one or two numbers.\nIf you chose to enter one simply hit enter again after you enter your first number.");
+            int requiredNumber = Convert.ToInt32(Console.ReadLine());
+            string secondInput = Console.ReadLine();
+            //This if statment checks to see if the there anything was entered for the second input.
+            if (secondInput != "")
+            {
+                int optionalNumber = Convert.ToInt32(secondInput);
+                Console.WriteLine("The result is: " + times3.MultiplyBy3(requiredNumber, optionalNumber));
+            }
+            else
+            {
+                Console.WriteLine("The result is: " + times3.MultiplyBy3(requiredNumber));
+            }
+            Console.ReadLine();
+        }
+    }
+}
