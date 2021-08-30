ed705d20124d3b9fe273017810db1c64cf4ce627
JoshOtter <87107050+JoshOtter@users.noreply.github.com>
2021-08-30 15:31:07 -0500

    Created cs files for Main Method assignment
    I created the main program and class files using overloaded methods to do math operations on int, float, and string data types.

---
+++ MainMethodAssignment/Addition.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodAssignment
+{
+    class Addition
+    {
+        public int Add(int intValue)
+        {
+            int result = intValue + 10;
+            return result;
+        }
+        public float Add(float floatValue)
+        {
+            float result = floatValue + 100;
+            return result;
+        }
+        public string Add(string stringValue)
+        {
+            int argument = Convert.ToInt32(stringValue);
+            int result = argument + 1000;
+            return result.ToString();
+        }
+    }
+}
---
+++ MainMethodAssignment/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Addition add10 = new Addition();
+            Console.WriteLine("Enter an integer.");
+            int intValue = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(add10.Add(intValue));
+
+            Console.WriteLine("Enter a decimal number.");
+            float floatValue = float.Parse(Console.ReadLine());
+            Console.WriteLine(add10.Add(floatValue));
+
+            Console.WriteLine("Enter a number.");
+            string stringValue = Console.ReadLine();
+            Console.WriteLine(add10.Add(stringValue));
+            Console.ReadLine();
+        }
+    }
+}
