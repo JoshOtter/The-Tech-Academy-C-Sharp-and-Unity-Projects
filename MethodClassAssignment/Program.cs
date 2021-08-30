0d3fa29daeebf31b15f14ed0611c08194ffef157
JoshOtter <87107050+JoshOtter@users.noreply.github.com>
2021-08-30 17:02:56 -0500

    Created cs files for the Method Class assignment
    I created a class with a method that uses named parameters.

---
+++ MethodClassAssignment/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TwoParameters twoNumbers = new TwoParameters();
+            twoNumbers.Calculate(firstNumber: 3,secondNumber: 5);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MethodClassAssignment/TwoParameters.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassAssignment
+{
+    public class TwoParameters
+    {
+        public void Calculate(int firstNumber, int secondNumber)
+        {
+            int result = firstNumber * 3;
+            Console.WriteLine(secondNumber);
+        }
+    }
+}
