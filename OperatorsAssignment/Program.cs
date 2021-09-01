58402cb80c3f3a4fa60e0840a59c9fb9c1ff008c
JoshOtter <87107050+JoshOtter@users.noreply.github.com>
2021-09-01 11:39:43 -0500

    Changed the comparison to employee.Id

--- OperatorsAssignment/Program.cs
+++ OperatorsAssignment/Program.cs
@@ -13,7 +13,7 @@ namespace OperatorsAssignment
             Employee employee1 = new Employee() { FirstName = "John", LastName = "Connor", Id = 00001 };
             Employee employee2 = new Employee() { FirstName = "Sarah", LastName = "Connor", Id = 00000 };
 
-            Console.WriteLine(employee1 == employee2);
+            Console.WriteLine(employee1.Id == employee2.Id);
             Console.ReadLine();
         }
     }
