bd781b7dad5c2e2c6f71c35356cb7eadcb16cb4c
JoshOtter <87107050+JoshOtter@users.noreply.github.com>
2021-09-01 06:52:07 -0500

    Created cs files for Operator assignment
    I overloaded the == operator to check for equality in the Id property between two employee objects.

---
+++ OperatorsAssignment/Employee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorsAssignment
+{
+    public class Employee : Person
+    {
+        public int Id { get; set; }
+
+        public static bool operator== (Employee employee1, Employee employee2)
+        {
+            bool result = employee1.Id == employee2.Id;
+            return result;
+        }
+
+        public static bool operator!= (Employee employee1, Employee employee2)
+        {
+            bool result = employee1.Id != employee2.Id;
+            return result;
+        }
+    }
+}
---
+++ OperatorsAssignment/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorsAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee1 = new Employee() { FirstName = "John", LastName = "Connor", Id = 00001 };
+            Employee employee2 = new Employee() { FirstName = "Sarah", LastName = "Connor", Id = 00000 };
+
+            Console.WriteLine(employee1 == employee2);
+            Console.ReadLine();
+        }
+    }
+}
