facf3e9912dff5ac11fedfe04bac9c2a68e1c447
JoshOtter <87107050+JoshOtter@users.noreply.github.com>
2021-09-02 12:05:19 -0500

    Created cs file for Parsing Enums assignment
    I created an enum for the days of the week and asked the user to input the current day. I then ran an if statement to check if the value could be converted to an int. If it could then , it sends a message to the user to enter a day of the week (I did this because entering numbers 0-6 also related to the enum values). If it couldn't be converted to an integer, then it parses the answer to the DayOfTheWeek enum and prints the results to the console. The entire set of code is set in a try catch in case the user misspells the day or enters something other than the specified weekday names or an integer. Should the user enter another value, the catch block tells them to enter a day of the week.

---
+++ ParsingEnumsAssignment/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParsingEnumsAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Please enter the current day of the week.");
+                string answer = Console.ReadLine();
+                int a = 0;
+                if (int.TryParse(answer, out a))
+                {
+                    Console.WriteLine("Please enter a day of the week.");
+                }
+                else
+                {
+                    DaysOfTheWeek today = (DaysOfTheWeek)Enum.Parse(typeof(DaysOfTheWeek), answer);
+                    Console.WriteLine("Today is " + today);
+                }                
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine("Please enter a day of the week.");
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+            
+
+        }
+        public enum DaysOfTheWeek
+        {
+            Sunday,
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday
+        }
+    }
+}
