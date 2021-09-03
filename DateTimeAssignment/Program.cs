e435198e18d0cd33ead32a6cdcc982878381f6cf
JoshOtter <87107050+JoshOtter@users.noreply.github.com>
2021-09-03 14:59:01 -0500

    Created cs file for the DateTime assignment
    I created a file that retrieves and prints the current DateTime to the console and then asks the user to enter a number. That number is then calculated into a TimeSpan as hours and added to the current DateTime and printed to the console to show the exact DateTime that many hours later.

---
+++ DateTimeAssignment/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTimeAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime date = DateTime.Now;
+            Console.WriteLine("It is currently: " + date + "\nEnter a number.");
+            int hour = Convert.ToInt32(Console.ReadLine());
+            TimeSpan duration = new TimeSpan(hour, 0, 0);
+            Console.WriteLine("In " + hour + " hours, it will be " + (date + duration));
+            Console.ReadLine();
+        }
+    }
+}
